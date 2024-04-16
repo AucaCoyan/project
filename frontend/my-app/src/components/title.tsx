25ab73b4dcc9f9ed3a0877542bec6118e6a68709
Sofía Fornaroli <Sofia0101>
2024-04-16 15:12:26 -0300

    agregando primeros components

--- frontend/my-app/src/components/title.tsx
+++ frontend/my-app/src/components/title.tsx
@@ -1,17 +1,7 @@
-import { Bebas_Neue } from 'next/font/google';
-
-
-const ubuntu = Bebas_Neue({
-    weight: '400',
-    subsets: ['latin'],
-  })
-  
-
-
 export default function Title() {
     return (
-        <div className={ubuntu.className}>
-        <h1 className="mx-7 mb-20 mt-9 text-3xl text-gray-800">Bite</h1>
-      </div>
+  
+        <h1 className="title-page">BITE</h1>
+      
     );
   }
