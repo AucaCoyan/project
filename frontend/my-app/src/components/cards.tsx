c2bd8d57d514fcf6df963db71c9a1e187bd4fa54
Sofía Fornaroli <Sofia0101>
2024-06-04 00:31:15 -0300

    :zap: haciendo  responsive cards.tsx

--- frontend/my-app/src/components/cards.tsx
+++ frontend/my-app/src/components/cards.tsx
@@ -1,39 +1,301 @@
 import Image from "next/image";
 
-
-
-
 export default function Card() {
   return (
+    <section className="ms:grid-cols-[repeat(2,50%)]  grid">
+      <div className="w-full m-auto grid lg:w-[1024px] min-[400px]:grid-cols-[repeat(2,50%)] md:grid-cols-[repeat(3,33.3%)] lg:grid-cols-[repeat(4,25%)] xl:w-[1200px]">
 
-<div className="flex-row flex flex-wrap justify-center">
-    <a href="#">
-        <Image className="m-4 aspect-square " width={300} height={300} src="/img/plato1.jpg" alt="" />
-    </a>
-    <a href="#">
-        <Image className="m-4 aspect-square" width={300} height={300} src="/img/plato2.jpg" alt="" />
-    </a>
-    <a href="#">
-        <Image className="m-4 aspect-square" width={300} height={300} src="/img/plato3.jpg" alt="" />
-    </a>
-    <a href="#">
-        <Image className="m-4 aspect-square" width={300} height={300} src="/img/plato4.jpg" alt="" />
-    </a>
-    <a href="#">
-        <Image className="m-4 aspect-square" width={300} height={300} src="/img/plato5.jpg" alt="" />
-    </a>
-    <a href="#">
-        <Image className="m-4 aspect-square" width={300} height={300} src="/img/plato6.jpg" alt="" />
-    </a>
-    <a href="#">
-        <Image className="m-4 aspect-square" width={300} height={300} src="/img/plato7.jpg" alt="" />
-    </a>
-    <a href="#">
-        <Image className="m-4 aspect-square" width={300} height={300} src="/img/plato8.jpg" alt="" />
-    </a>
-   
-   
-   
+        <div className="m-[5px]">
+
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato1.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato2.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato3.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato4.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato5.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className=" w-full"
+              width={300}
+              height={300}
+              src="/img/plato6.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato7.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato8.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="m-[5px]">
+
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato1.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato2.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato3.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato4.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato5.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className=" w-full"
+              width={300}
+              height={300}
+              src="/img/plato6.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato7.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <div className="m-[5px]">
+          <a href="#">
+            <Image
+              className="w-full"
+              width={300}
+              height={300}
+              src="/img/plato8.jpg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="m-[5px]">
+
+<a href="#">
+  <Image
+    className="w-full"
+    width={300}
+    height={300}
+    src="/img/plato1.jpg"
+    alt=""
+  />
+</a>
 </div>
-  )
+
+<div className="m-[5px]">
+<a href="#">
+  <Image
+    className="w-full"
+    width={300}
+    height={300}
+    src="/img/plato2.jpg"
+    alt=""
+  />
+</a>
+</div>
+
+<div className="m-[5px]">
+<a href="#">
+  <Image
+    className="w-full"
+    width={300}
+    height={300}
+    src="/img/plato3.jpg"
+    alt=""
+  />
+</a>
+</div>
+
+<div className="m-[5px]">
+<a href="#">
+  <Image
+    className="w-full"
+    width={300}
+    height={300}
+    src="/img/plato4.jpg"
+    alt=""
+  />
+</a>
+</div>
+
+<div className="m-[5px]">
+<a href="#">
+  <Image
+    className="w-full"
+    width={300}
+    height={300}
+    src="/img/plato5.jpg"
+    alt=""
+  />
+</a>
+</div>
+
+<div className="m-[5px]">
+<a href="#">
+  <Image
+    className=" w-full"
+    width={300}
+    height={300}
+    src="/img/plato6.jpg"
+    alt=""
+  />
+</a>
+</div>
+
+<div className="m-[5px]">
+<a href="#">
+  <Image
+    className="w-full"
+    width={300}
+    height={300}
+    src="/img/plato7.jpg"
+    alt=""
+  />
+</a>
+</div>
+
+<div className="m-[5px]">
+<a href="#">
+  <Image
+    className="w-full"
+    width={300}
+    height={300}
+    src="/img/plato8.jpg"
+    alt=""
+  />
+</a>
+</div>
+
+       
+      </div>
+    </section>
+  );
 }
