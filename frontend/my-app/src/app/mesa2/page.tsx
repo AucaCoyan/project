e4c933b47da40cbb4d81ef4c343eeb115edf262b
Sofía Fornaroli <91976946+Sofia0101@users.noreply.github.com>
2024-07-22 17:29:00 -0300

    termino qr (#10)

--- frontend/my-app/src/app/mesa2/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Mesa2() {
-    return (
-        <>
-        <h1>Mesa 2</h1>
-        </>
-    )
-}
--- frontend/my-app/src/app/page.tsx
+++ frontend/my-app/src/app/page.tsx
@@ -14,25 +14,32 @@ export default function Home() {
       <Card />
 
       <div className="m-4 ml-[3.7rem] flex">
+        <h1>Qr para comensal</h1><br />
         <QRCode
           style={{ height: "100px", width: "100px", margin: "20px" }}
-          value="https://bite-ruby.vercel.app/comensal"
+          value="http://localhost:3000/1"
         />
+        <br />
 
 
-
+        <h1>Qr para caja</h1><br />
         <QRCode
           size={256}
           style={{ height: "100px", width: "100px", margin: "20px" }}
-          value="https://bite-ruby.vercel.app/caja"
+          value="http://localhost:3000/comensal"
           
         />
+        <br />
+
+         <h1>Qr para mozo</h1>
+         <br />
         <QRCode
           size={256}
           style={{ height: "100px", width: "100px", margin: "20px" }}
           value="https://bite-ruby.vercel.app/mozo"
           
         />
+        <br />
         
 
         
