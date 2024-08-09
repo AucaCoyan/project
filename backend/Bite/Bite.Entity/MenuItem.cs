ad54a722abee9cff017f316ec1f01f5b47a5d881
NachoMies <ignaciomies@live.com.ar>
2024-08-08 23:19:23 -0300

    Endpoints de OrderItem y Order
    Solo Gets y Post

--- backend/Bite/Bite.Entity/MenuItem.cs
+++ backend/Bite/Bite.Entity/MenuItem.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public int Type { get; set; }
+        public MenuItemType Type { get; set; }
         public Restaurant Restaurant { get; set; }
     }
 
